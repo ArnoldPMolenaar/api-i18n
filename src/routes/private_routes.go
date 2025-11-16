4db1ce33aac49c1723870a002592a3b8e2a06eeb
Arnold Molenaar <arnoldp1991@gmail.com>
2025-11-16 12:09:18 +0100

    ✨ feat: Initial commit of an empty API to start coding
    Changelog: feature

---
+++ src/routes/private_routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/ArnoldPMolenaar/api-utils/middleware"
+	"github.com/gofiber/fiber/v2"
+)
+
+// PrivateRoutes func for describe group of private routes.
+func PrivateRoutes(a *fiber.App) {
+	// Create private routes group.
+	route := a.Group("/v1")
+
+	// Register test route
+	route.Get("/test", middleware.MachineProtected())
+}
